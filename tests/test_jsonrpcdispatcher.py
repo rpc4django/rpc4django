8fbed81e9771e7b27475fcd742edf33a4b783a01
Albert Hopkins <marduk@python.net>
2013-04-15 11:33:21 -0400

    Add "jsonrpc" member to JSON-RPC responce.
    According to the JSON-RPC spec[1], the response should have the
    "jsonrpc" member whose value should be "2.0".
    
    [1] http://www.jsonrpc.org/specification#response_object

--- tests/test_jsonrpcdispatcher.py
+++ tests/test_jsonrpcdispatcher.py
@@ -15,21 +15,21 @@ class TestJSONRPCDispatcher(unittest.TestCase):
     def setUp(self):
         def add(a,b):
             return a+b
-        
+
         def factorial(num):
             if num > 1:
                 return num * factorial(num-1)
             else:
                 return 1
-            
+
         def unicode_ret():
             return u'はじめまして'
-        
+
         def kwargstest(a, b, **kwargs):
             if kwargs.get('c', None) is not None:
                 return True
             return False
-        
+
         self.dispatcher = JSONRPCDispatcher()
         self.dispatcher.register_function(add, 'add')
         self.dispatcher.register_function(factorial, 'fact')
@@ -57,17 +57,29 @@ class TestJSONRPCDispatcher(unittest.TestCase):
         self.assertEqual(type(jsondict['result']), unicode)
         self.assertEqual(type(datetime.strptime(jsondict['result'], "%Y-%m-%d %H:%M:%S")), datetime)
 
+    def test_jsonrpc_member(self):
+        # Demonstrate that the jsondict contains "jsonrpc"
+        d = dict()
+        d['params'] = [1,2]
+        d['method'] = 'kwargstest'
+        d['id'] = 1
+
+        jsontxt = json.dumps(d)
+        resp = self.dispatcher.dispatch(jsontxt)
+        jsondict = json.loads(resp)
+        self.assertEqual(jsondict['jsonrpc'], '2.0')
+
     def test_kwargs(self):
         d = dict()
         d['params'] = [1,2]
         d['method'] = 'kwargstest'
         d['id'] = 1
-        
+
         jsontxt = json.dumps(d)
         resp = self.dispatcher.dispatch(jsontxt)
         jsondict = json.loads(resp)
         self.assertFalse(jsondict['result'])
-        
+
         resp = self.dispatcher.dispatch(jsontxt, c=1)
         jsondict = json.loads(resp)
         self.assertTrue(jsondict['result'])
@@ -79,14 +91,14 @@ class TestJSONRPCDispatcher(unittest.TestCase):
         self.assertTrue(jsondict['error'] is None)
         self.assertEqual(jsondict['id'], 1)
         self.assertEqual(jsondict['result'], u'はじめまして')
-        
-        jsontxt = '{"params":["\u306f\u3058\u3081","\u307e\u3057\u3066"],"method":"add","id":2}' 
+
+        jsontxt = '{"params":["\u306f\u3058\u3081","\u307e\u3057\u3066"],"method":"add","id":2}'
         resp = self.dispatcher.dispatch(jsontxt)
         jsondict = json.loads(resp)
         self.assertTrue(jsondict['error'] is None)
         self.assertEqual(jsondict['id'], 2)
         self.assertEqual(jsondict['result'], u'はじめまして')
-        
+
     def test_dispatch_success(self):
         jsontxt = '{"params":[1,2],"method":"add","id":1}'
         resp = self.dispatcher.dispatch(jsontxt)
@@ -94,14 +106,14 @@ class TestJSONRPCDispatcher(unittest.TestCase):
         self.assertTrue(jsondict['error'] is None)
         self.assertEqual(jsondict['id'], 1)
         self.assertEqual(jsondict['result'], 3)
-        
+
         jsontxt = '{"params":[5],"method":"fact","id":"hello"}'
         resp = self.dispatcher.dispatch(jsontxt)
         jsondict = json.loads(resp)
         self.assertTrue(jsondict['error'] is None)
         self.assertEqual(jsondict['id'], 'hello')
         self.assertEqual(jsondict['result'], 120)
-        
+
     def test_method_error(self):
         jsontxt = '{"params":["a"],"method":"fact","id":"hello"}'
         resp = self.dispatcher.dispatch(jsontxt)
@@ -125,7 +137,7 @@ class TestJSONRPCDispatcher(unittest.TestCase):
         self.assertTrue(isinstance(jsondict['error'], dict))
         self.assertEqual(jsondict['error'] ['code'], 105)
         self.assertEqual(jsondict['error'] ['message'], 'method "add123" is not supported')
-        
+
     def test_dispatch_badrequest(self):
         jsontxt = '"params":asdf[14]","method":"add","id":0}'
         resp = self.dispatcher.dispatch(jsontxt)
@@ -135,7 +147,7 @@ class TestJSONRPCDispatcher(unittest.TestCase):
         self.assertTrue(isinstance(jsondict['error'], dict))
         self.assertEqual(jsondict['error'] ['code'], 101)
         self.assertEqual(jsondict['error'] ['message'], 'JSON decoding error')
-        
+
         jsontxt = '["should", "be", "a", "Object"]'
         resp = self.dispatcher.dispatch(jsontxt)
         jsondict = json.loads(resp)
@@ -144,7 +156,7 @@ class TestJSONRPCDispatcher(unittest.TestCase):
         self.assertTrue(isinstance(jsondict['error'], dict))
         self.assertEqual(jsondict['error'] ['code'], 102)
         self.assertEqual(jsondict['error'] ['message'], 'Cannot decode to a javascript Object')
-        
+
         jsontxt = '{"params":"shouldbelist","method":"add","id":0}'
         resp = self.dispatcher.dispatch(jsontxt)
         jsondict = json.loads(resp)
@@ -153,7 +165,7 @@ class TestJSONRPCDispatcher(unittest.TestCase):
         self.assertTrue(isinstance(jsondict['error'], dict))
         self.assertEqual(jsondict['error'] ['code'], 102)
         self.assertEqual(jsondict['error'] ['message'], 'params must be a javascript Array')
-        
+
         jsontxt = '{"params":"[]","method":123,"id":42}'
         resp = self.dispatcher.dispatch(jsontxt)
         jsondict = json.loads(resp)
