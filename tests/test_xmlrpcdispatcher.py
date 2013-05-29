20d08f2023fd051e66bbe212aa5f76aebf7266fb
David Fischer <djfische@gmail.com>
2013-05-28 21:34:00 -0700

    Added python3 compatibility

--- tests/test_xmlrpcdispatcher.py
+++ tests/test_xmlrpcdispatcher.py
@@ -5,8 +5,13 @@ XML RPC Dispatcher Tests
 '''
 
 import unittest
-import xmlrpclib
 from rpc4django.xmlrpcdispatcher import *
+
+try:
+    from xmlrpclib import loads, dumps
+except ImportError:
+    from xmlrpc.client import loads, dumps
+
 
 class TestXMLRPCDispatcher(unittest.TestCase):
 
@@ -15,19 +20,20 @@ class TestXMLRPCDispatcher(unittest.TestCase):
             if kwargs.get('c', None) is not None:
                 return True
             return False
-        
+
         self.dispatcher = XMLRPCDispatcher()
         self.dispatcher.register_function(kwargstest, 'kwargstest')
 
     def test_kwargs(self):
-        xml = xmlrpclib.dumps((1,2), 'kwargstest')
+        xml = dumps((1,2), 'kwargstest')
         ret = self.dispatcher.dispatch(xml)
-        out, name = xmlrpclib.loads(ret)
+        out, name = loads(ret)
         self.assertFalse(out[0])
-        
+
         ret = self.dispatcher.dispatch(xml, c=1)
-        out, name = xmlrpclib.loads(ret)
+        out, name = loads(ret)
         self.assertTrue(out[0])
-        
+
+
 if __name__ == '__main__':
     unittest.main()
