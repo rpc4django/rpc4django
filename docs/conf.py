7a9e4ced152e439920e55da235e5efc66553a27c
David Fischer <djfische@gmail.com>
2013-05-30 08:04:52 -0700

    Version 0.2.3 :sparkles:

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ copyright = u'%d, the respective authors' %datetime.now().year
 # built documents.
 #
 # The short X.Y version.
-version = '0.2.2'
+version = '0.2.3'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ RPC4Django Documentation
 
 setup(
     name='rpc4django',
-    version='0.2.2',
+    version='0.2.3',
     description='Handles JSONRPC and XMLRPC requests easily with Django',
     long_description=long_description,
     author='David Fischer',
