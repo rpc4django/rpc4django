f534a820178d194b2ebea866ce5558e87f8f8a71
David Fischer <djfische@gmail.com>
2013-05-29 20:06:46 -0700

    Unicode fix

--- example/testapp/__init__.py
+++ example/testapp/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from rpc4django import rpcmethod
-from othermodule import intro, request
-from secretmodule import * # imports protected method
+from .othermodule import intro, request
+from .secretmodule import * # imports protected method
 
 @rpcmethod(name='rpc4django.mytestmethod',signature=['int', 'int', 'int', 'int'])
 def mytestmethod(a, b, c):
