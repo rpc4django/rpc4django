64473d56519180bd5d1beb070b281b9d848c143d
David Fischer <djfische@gmail.com>
2015-12-05 21:04:08 -0800

    Handle incompatible versions of Python and Django

--- runtravis.sh
+++ runtravis.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -ev
-if [ $TRAVIS_PYTHON_VERSION == '2.6' ] && ([ $DJANGO == 'Django<1.8' ] || [ $DJANGO == 'Django<1.9' ])
-then 
-   exit 0 
-elif ([ $TRAVIS_PYTHON_VERSION == '3.3'] || [ $TRAVIS_PYTHON_VERSION == '3.4' ]) && \ 
-    ([ $DJANGO == 'Django<1.4' ] || [ $DJANGO == 'Django<1.5' ])
+
+# Handle incompatible versions of Python & Django
+if ([ $TRAVIS_PYTHON_VERSION == '3.3' ] && [ $DJANGO == 'Django<1.10' ])
 then
     exit 0
 else
