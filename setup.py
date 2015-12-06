179765d611c4ca54f265c45c890c3dfe8b974adf
David Fischer <djfische@gmail.com>
2015-12-05 20:38:03 -0800

    Update required and recommended versions

--- setup.py
+++ setup.py
@@ -44,15 +44,16 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 
-    install_requires=['Django >= 1.3', 'defusedxml'],
+    install_requires=['Django >= 1.4', 'defusedxml'],
     # tests_require=['Django >= 1.3'],
     extras_require={
         "reST": ['docutils >= 0.4'],
